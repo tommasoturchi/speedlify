a7a7407f4c56300b2ac827d0869bf233795e4f4c
Tommaso Turchi <apps@tommasoturchi.fyi>
2024-01-09 16:25:16 +0100

    Update site configurations in 19.js and trx.js

---
+++ _data/sites/19.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "19.studio", // optional, falls back to object key
+  description: "19.Studio Sites",
+  options: {
+    runs: 1,
+    frequency: 1, // (in minutes)
+  },
+  urls: ["https://astra.19.studio", "https://backdoor.fi"],
+};
--- _data/sites/trx.js
+++ _data/sites/trx.js
@@ -1,12 +1,13 @@
 module.exports = {
-	name: "trx.li", // optional, falls back to object key
-	description: "The sites that get tested",
-	options: {
-		runs: 1,
-		frequency: 1, // (in minutes)
-	},
-	urls: [
-		"https://axai.trx.li/",
-		"https://synergy.trx.li/"
-	]
+  name: "trx.li", // optional, falls back to object key
+  description: "trx.li Sites",
+  options: {
+    runs: 1,
+    frequency: 1, // (in minutes)
+  },
+  urls: [
+    "https://axai.trx.li",
+    "https://synergy.trx.li",
+    "https://d3lab.netlify.app",
+  ],
 };
